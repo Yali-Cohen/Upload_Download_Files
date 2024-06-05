e78e3d54135ba63c93410778c020fd33dbe1a168
Yali Cohen <yhlyk8498@gmail.com>
2024-06-05 20:10:54 +0300

    test

--- test.py
+++ test.py
@@ -11,7 +11,9 @@ num_to_binary(5, 8)
 
 def num_to_negBin(bin_num,bits):
     for i in range(bits):#not to the number
-        if bin_num[i] == 0:
-            bin_num[i] = 1
+        if bin_num[i] == '0':
+            bin_num[i] = bin_num[:i] + "1" + bin_num[i+1:]
         else:
-            bin_num[i] = 0
+            bin_num[i] = '0'
+    print(bin_num)
+num_to_negBin(num_to_binary(5, 8),8)
---
+++ youngfortech.py
@@ -0,0 +1,16 @@
+
+
+    
+
+def num_to_binary(number, bits):
+    bin_num = ""
+    for bit in range(bits - 1, -1, -1):
+        if number >= 2 ** bit:
+            bin_num += '1'
+            number -= 2 ** bit
+        else:
+            bin_num += '0'
+    print(bin_num)
+
+
+num_to_binary(5, 8)
