80b837f46036ce36f14fa7d0b43dae44a18a0ae5
Yali Cohen <yhlyk8498@gmail.com>
2024-06-24 20:28:32 +0300

    frequent words

--- frequentWord.py
+++ frequentWord.py
@@ -1,22 +1,29 @@
+import sys
+
 def readFile():
+    if len(sys.argv) < 3:
+        sys.exit(1)
+    
     file_path = createFile()
     with open(file_path, 'r') as file:
         content = file.read()
         words = content.split()
     list_to_dictionary(words)
+
 def createFile():
     try:
-        file_path = input_file()
-    except KeyboardInterrupt:
-        print("wrong file name")
-    except FileNotFoundError:
-        print("this file isn't exist")
-    with open(file_path,'w') as file:
+        if len(sys.argv) > 1:
+            file_path = sys.argv[1]
+        else:
+            file_path = input("Enter the path of the file you want: ")
+    except IndexError:
+        print("No file name provided")
+        sys.exit(1)
+    
+    with open(file_path, 'w') as file:
         file.write("hello world, my name is yali and my name is levy levy levy")
     return file_path
-def input_file():
-    file_path = input("Enter the path of the file you want ")
-    return file_path
+
 def list_to_dictionary(words):
     counter = {}
     for word in words:
@@ -24,11 +31,23 @@ def list_to_dictionary(words):
             counter[word] += 1
         else:
             counter[word] = 1
-    print(counter)
-    items = counter.items()
-    sorted_items = sorted(counter.items(), key=lambda item: item[1], reverse=True)
-    print(sorted_items)
-readFile()
-
-
     
+    sorted_items = sorted(counter.items(), key=lambda item: item[1], reverse=True)
+    try:
+        if len(sys.argv) > 2:
+            N = int(sys.argv[2])
+        else:
+            N = int(input("Enter the number of top words to display: "))
+    except IndexError:
+        print("No number provided for N")
+        sys.exit(1)
+    except ValueError:
+        print("Invalid number provided for N")
+        sys.exit(1)
+    
+    print(f"Top {N} words:")
+    for i in range(min(N, len(sorted_items))):
+        print(sorted_items[i][0])
+
+if __name__ == "__main__":
+    readFile()
