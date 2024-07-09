d39f790e4513644ab62a0f9286b7a496e20da699
Yali Cohen <yhlyk8498@gmail.com>
2024-07-09 21:19:44 +0300

    final commit

---
+++ Upload_Download_Files/clientUpDw.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST = '127.0.0.1' 
+PORT = 65432
+
+choice = input("Enter what do you want, download or upload a file: ").lower()
+if choice == "upload":
+    path = input("To upload, please provide the path of your file: ")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        request = choice + "|" + path
+        s.sendall(request.encode('utf-8'))
+        data = s.recv(1024)
+elif choice == "download": 
+    path = input("To download, please provide the file name: ")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        request = choice + "|" + path
+        s.sendall(request.encode('utf-8'))
+        data = s.recv(1024)
+else:
+    print("Invalid choice")
+    data = None
+
+if data:
+    print(data.decode('utf-8'))
